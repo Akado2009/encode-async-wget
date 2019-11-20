5c8ab59319f956d048a8760729d111404afae90b
Akado2009 <prosvirov.k@gmail.com>
2019-11-20 18:26:26 +0300

    ss

---
+++ additional/wget_no_cl_commands.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001GYT/', '/files/ENCFF001GYU/', '/files/ENCFF001GZF/', '/files/ENCFF798MEL/', '/files/ENCFF436GDS/', '/files/ENCFF345HOL/', '/files/ENCFF972ATQ/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001GYT/', '/files/ENCFF001GYU/', '/files/ENCFF001GZF/', '/files/ENCFF798MEL/', '/files/ENCFF436GDS/', '/files/ENCFF345HOL/', '/files/ENCFF972ATQ/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF798MEL/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000CIN/', '/files/ENCFF000CIO/', '/files/ENCFF000CIP/', '/files/ENCFF000CIU/', '/files/ENCFF000CIW/', '/files/ENCFF090CYH/', '/files/ENCFF138XPE/', '/files/ENCFF898XQK/', '/files/ENCFF860HKY/', '/files/ENCFF955HWE/', '/files/ENCFF412GJA/', '/files/ENCFF859VNG/', '/files/ENCFF204ZPV/', '/files/ENCFF092GQD/', '/files/ENCFF753JIU/', '/files/ENCFF615VVU/', '/files/ENCFF487DBD/', '/files/ENCFF782IQQ/', '/files/ENCFF739WQP/', '/files/ENCFF005RXL/', '/files/ENCFF429XTA/', '/files/ENCFF638YZB/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000CIN/', '/files/ENCFF000CIO/', '/files/ENCFF000CIP/', '/files/ENCFF000CIU/', '/files/ENCFF000CIW/', '/files/ENCFF090CYH/', '/files/ENCFF138XPE/', '/files/ENCFF898XQK/', '/files/ENCFF860HKY/', '/files/ENCFF955HWE/', '/files/ENCFF412GJA/', '/files/ENCFF859VNG/', '/files/ENCFF204ZPV/', '/files/ENCFF092GQD/', '/files/ENCFF753JIU/', '/files/ENCFF615VVU/', '/files/ENCFF487DBD/', '/files/ENCFF782IQQ/', '/files/ENCFF739WQP/', '/files/ENCFF005RXL/', '/files/ENCFF429XTA/', '/files/ENCFF638YZB/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF204ZPV/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HJU/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001HJU/', '/files/ENCFF001HJY/', '/files/ENCFF001HKF/', '/files/ENCFF597YIM/', '/files/ENCFF632ZLA/', '/files/ENCFF489TFZ/', '/files/ENCFF092GCH/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF092GCH/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000CQE/', '/files/ENCFF000CQF/', '/files/ENCFF000CQH/', '/files/ENCFF000CQO/', '/files/ENCFF000CQP/', '/files/ENCFF554LJZ/', '/files/ENCFF681UKF/', '/files/ENCFF386PAD/', '/files/ENCFF007RSP/', '/files/ENCFF923PUU/', '/files/ENCFF208SHP/', '/files/ENCFF552KRT/', '/files/ENCFF250VFW/', '/files/ENCFF579ZCP/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001HQW/', '/files/ENCFF001HQX/', '/files/ENCFF001HRI/', '/files/ENCFF067VME/', '/files/ENCFF741CYK/', '/files/ENCFF468MCN/', '/files/ENCFF138KCG/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF208SHP/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HWM/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF671FKH/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF870RFE/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000CLV/', '/files/ENCFF000CLW/', '/files/ENCFF000CLX/', '/files/ENCFF000CMD/', '/files/ENCFF000CMJ/', '/files/ENCFF604QBU/', '/files/ENCFF292GHV/', '/files/ENCFF221LXY/', '/files/ENCFF377CTF/', '/files/ENCFF516NPM/', '/files/ENCFF050XUG/', '/files/ENCFF288LLD/', '/files/ENCFF587LRA/', '/files/ENCFF715NGU/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000CLV/', '/files/ENCFF000CLW/', '/files/ENCFF000CLX/', '/files/ENCFF000CMD/', '/files/ENCFF000CMJ/', '/files/ENCFF604QBU/', '/files/ENCFF292GHV/', '/files/ENCFF221LXY/', '/files/ENCFF377CTF/', '/files/ENCFF516NPM/', '/files/ENCFF050XUG/', '/files/ENCFF288LLD/', '/files/ENCFF587LRA/', '/files/ENCFF715NGU/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000CLV/', '/files/ENCFF000CLW/', '/files/ENCFF000CLX/', '/files/ENCFF000CMD/', '/files/ENCFF000CMJ/', '/files/ENCFF604QBU/', '/files/ENCFF292GHV/', '/files/ENCFF221LXY/', '/files/ENCFF377CTF/', '/files/ENCFF516NPM/', '/files/ENCFF050XUG/', '/files/ENCFF288LLD/', '/files/ENCFF587LRA/', '/files/ENCFF715NGU/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000BJZ/', '/files/ENCFF000BKA/', '/files/ENCFF000BKD/', '/files/ENCFF000BKI/', '/files/ENCFF000BKK/', '/files/ENCFF009XHG/', '/files/ENCFF298NLV/', '/files/ENCFF791XAM/', '/files/ENCFF698ZZD/', '/files/ENCFF208RCO/', '/files/ENCFF325AJA/', '/files/ENCFF130ENW/', '/files/ENCFF003XTF/', '/files/ENCFF589EFJ/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF000BKA/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000BJZ/', '/files/ENCFF000BKA/', '/files/ENCFF000BKD/', '/files/ENCFF000BKI/', '/files/ENCFF000BKK/', '/files/ENCFF009XHG/', '/files/ENCFF298NLV/', '/files/ENCFF791XAM/', '/files/ENCFF698ZZD/', '/files/ENCFF208RCO/', '/files/ENCFF325AJA/', '/files/ENCFF130ENW/', '/files/ENCFF003XTF/', '/files/ENCFF589EFJ/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HON/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HOC/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001HUZ/', '/files/ENCFF001HVC/', '/files/ENCFF001HVP/', '/files/ENCFF707TMD/', '/files/ENCFF700CVK/', '/files/ENCFF685YFH/', '/files/ENCFF162IBF/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HKI/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001HJZ/', '/files/ENCFF001HKA/', '/files/ENCFF001HKG/', '/files/ENCFF965YGZ/', '/files/ENCFF683AKU/', '/files/ENCFF699ETI/', '/files/ENCFF521ZBF/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HKH/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HIX/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF069XVM/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001HIW/', '/files/ENCFF001HIX/', '/files/ENCFF001HJJ/', '/files/ENCFF908OBD/', '/files/ENCFF069XVM/', '/files/ENCFF188FPI/', '/files/ENCFF743ECZ/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF280SOV/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HSZ/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HTK/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000VKL/', '/files/ENCFF000VKM/', '/files/ENCFF000VKO/', '/files/ENCFF000VKN/', '/files/ENCFF676VDZ/', '/files/ENCFF738UVB/', '/files/ENCFF519MQF/', '/files/ENCFF908XUV/', '/files/ENCFF814VRG/', '/files/ENCFF918JVX/', '/files/ENCFF176MNQ/', '/files/ENCFF522CTK/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000VKL/', '/files/ENCFF000VKM/', '/files/ENCFF000VKO/', '/files/ENCFF000VKN/', '/files/ENCFF676VDZ/', '/files/ENCFF738UVB/', '/files/ENCFF519MQF/', '/files/ENCFF908XUV/', '/files/ENCFF814VRG/', '/files/ENCFF918JVX/', '/files/ENCFF176MNQ/', '/files/ENCFF522CTK/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000VKL/', '/files/ENCFF000VKM/', '/files/ENCFF000VKO/', '/files/ENCFF000VKN/', '/files/ENCFF676VDZ/', '/files/ENCFF738UVB/', '/files/ENCFF519MQF/', '/files/ENCFF908XUV/', '/files/ENCFF814VRG/', '/files/ENCFF918JVX/', '/files/ENCFF176MNQ/', '/files/ENCFF522CTK/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF746GSV/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF501QRQ/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF421WFD/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF914LSY/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF000CCQ/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000CCJ/', '/files/ENCFF000CCL/', '/files/ENCFF000CCM/', '/files/ENCFF000CCQ/', '/files/ENCFF000CCW/', '/files/ENCFF383QCX/', '/files/ENCFF865NTT/', '/files/ENCFF545JWL/', '/files/ENCFF075YNC/', '/files/ENCFF695ZNW/', '/files/ENCFF914LSY/', '/files/ENCFF572RFU/', '/files/ENCFF701QJX/', '/files/ENCFF730IJS/', '/files/ENCFF692GVG/', '/files/ENCFF244XQF/', '/files/ENCFF365LFR/', '/files/ENCFF020SWF/', '/files/ENCFF894MBE/', '/files/ENCFF172HPC/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF145CAZ/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001HMG/', '/files/ENCFF001HMH/', '/files/ENCFF001HMS/', '/files/ENCFF008DOI/', '/files/ENCFF255XVF/', '/files/ENCFF643VRW/', '/files/ENCFF145CAZ/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001HMG/', '/files/ENCFF001HMH/', '/files/ENCFF001HMS/', '/files/ENCFF008DOI/', '/files/ENCFF255XVF/', '/files/ENCFF643VRW/', '/files/ENCFF145CAZ/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001HWX/', '/files/ENCFF001HWZ/', '/files/ENCFF001HXF/', '/files/ENCFF948UIP/', '/files/ENCFF682GMA/', '/files/ENCFF512ZCH/', '/files/ENCFF727MWW/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001HWX/', '/files/ENCFF001HWZ/', '/files/ENCFF001HXF/', '/files/ENCFF948UIP/', '/files/ENCFF682GMA/', '/files/ENCFF512ZCH/', '/files/ENCFF727MWW/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF682GMA/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HJK/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HJL/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HJV/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001HAK/', '/files/ENCFF001HAL/', '/files/ENCFF001HAV/', '/files/ENCFF443JRW/', '/files/ENCFF207FGO/', '/files/ENCFF939DCX/', '/files/ENCFF970LXF/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000AMC/', '/files/ENCFF000AMM/', '/files/ENCFF128SFZ/', '/files/ENCFF849QON/', '/files/ENCFF825GGQ/', '/files/ENCFF248YTS/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000AMC/', '/files/ENCFF000AMM/', '/files/ENCFF128SFZ/', '/files/ENCFF849QON/', '/files/ENCFF825GGQ/', '/files/ENCFF248YTS/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HKW/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001HKS/', '/files/ENCFF001HKW/', '/files/ENCFF001HLJ/', '/files/ENCFF293QZV/', '/files/ENCFF757OWU/', '/files/ENCFF769RBW/', '/files/ENCFF234EAH/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF769RBW/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HPW/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF747WXH/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001HPW/', '/files/ENCFF001HPX/', '/files/ENCFF001HQI/', '/files/ENCFF712TPV/', '/files/ENCFF272GNJ/', '/files/ENCFF747WXH/', '/files/ENCFF628PKL/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HLR/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HLF/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF783NTR/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF350LQZ/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF000CFK/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF000CFR/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001HRJ/', '/files/ENCFF001HRK/', '/files/ENCFF001HRU/', '/files/ENCFF047NAO/', '/files/ENCFF115YWW/', '/files/ENCFF735TUW/', '/files/ENCFF213ZXO/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001HRJ/', '/files/ENCFF001HRK/', '/files/ENCFF001HRU/', '/files/ENCFF047NAO/', '/files/ENCFF115YWW/', '/files/ENCFF735TUW/', '/files/ENCFF213ZXO/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HRK/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF699UTE/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF481RBK/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF917GFZ/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF463QOK/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HQK/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF327YGC/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000BQJ/', '/files/ENCFF000BQK/', '/files/ENCFF000BQM/', '/files/ENCFF000BQP/', '/files/ENCFF000BQS/', '/files/ENCFF000BQV/', '/files/ENCFF000BQW/', '/files/ENCFF780ZFP/', '/files/ENCFF261CBZ/', '/files/ENCFF771PFZ/', '/files/ENCFF039WIG/', '/files/ENCFF636OYP/', '/files/ENCFF496RFB/', '/files/ENCFF907AUP/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF001HSJ/', '/files/ENCFF001HSK/', '/files/ENCFF001HSV/', '/files/ENCFF745AMT/', '/files/ENCFF265HDZ/', '/files/ENCFF468CIV/', '/files/ENCFF242GIK/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF000BQW/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF801RTD/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000BGU/', '/files/ENCFF000BGV/', '/files/ENCFF000BGY/', '/files/ENCFF000BHE/', '/files/ENCFF000BHF/', '/files/ENCFF997HCS/', '/files/ENCFF556RIX/', '/files/ENCFF152UJF/', '/files/ENCFF702WJD/', '/files/ENCFF749GHL/', '/files/ENCFF913IHA/', '/files/ENCFF788YKS/', '/files/ENCFF813ZYQ/', '/files/ENCFF607BMM/', '/files/ENCFF417CEL/', '/files/ENCFF903ATW/', '/files/ENCFF801RTD/', '/files/ENCFF062WDT/', '/files/ENCFF115JNM/', '/files/ENCFF861GBM/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org['/files/ENCFF000BGU/', '/files/ENCFF000BGV/', '/files/ENCFF000BGY/', '/files/ENCFF000BHE/', '/files/ENCFF000BHF/', '/files/ENCFF997HCS/', '/files/ENCFF556RIX/', '/files/ENCFF152UJF/', '/files/ENCFF702WJD/', '/files/ENCFF749GHL/', '/files/ENCFF913IHA/', '/files/ENCFF788YKS/', '/files/ENCFF813ZYQ/', '/files/ENCFF607BMM/', '/files/ENCFF417CEL/', '/files/ENCFF903ATW/', '/files/ENCFF801RTD/', '/files/ENCFF062WDT/', '/files/ENCFF115JNM/', '/files/ENCFF861GBM/']@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HRW/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF001HRX/@@download
+wget -P /mnt/scratch/shared/SG_KIRILL/controls https://www.encodeproject.org/files/ENCFF419THA/@@download
